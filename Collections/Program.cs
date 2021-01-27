f9c24f343f2161f4af99fa39ef614399e7093784
Mehmet Can Çerçi <mehmetcerci36@gmail.com>
2021-01-27 15:47:54 +0300

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Alex","Smith","Al","Davis" };
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> {"Alex","Smith","Al","Davis" };
+            isimler2.Add("Mark");
+        }
+    }
+}
